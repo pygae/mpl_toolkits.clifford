a56d7844c84b97f9efe0bc438bc191b6ec1237f1
Eric Wieser <wieser.eric@gmail.com>
2020-02-07 14:20:03 +0000

    Refactor the plotter to allow heterogenous lists of objects
    These are now grouped together into homogeneous lists, for efficiency.
    
    Also switches to a `functools.singledispatch`-driven plotter, which makes it a little more natural to extend.
    
    Swapping these functions to use `yield` instead of `return some_list` also makes them easier to write

--- mpl_toolkits/clifford/__init__.py
+++ mpl_toolkits/clifford/__init__.py
@@ -1,15 +1,22 @@
+import math
+from typing import Iterator
+import itertools
+import collections
+import functools
+
 from matplotlib.artist import Artist
 from matplotlib.patches import Circle, FancyArrowPatch, Patch
 from matplotlib.collections import PatchCollection
 from matplotlib.tri import Triangulation
 from matplotlib.path import Path
-from clifford.tools import classify
-import clifford
+
 from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.mplot3d import proj3d, art3d
-import math
-from trimesh.creation import icosphere
+
 import numpy as np
+
+from clifford.tools import classify
+import clifford
 
 from ._version import __version__
 
@@ -18,6 +25,7 @@ __all__ = ['plot']
 
 def _line_style_for_radius(r):
     return '-' if r.imag == 0 else ":"
+
 
 def plot(ax, cga_objs, **kwargs):
     """
@@ -41,55 +49,84 @@ def plot(ax, cga_objs, **kwargs):
         axis_dims = 2
     obj_dims = layout.dims - 2  # for ei, eo
     if obj_dims == axis_dims == 2:
-        return _plot_2d(ax, layout, cga_objs, **kwargs)
+        return list(_Plotter2d(ax, layout).plot(cga_objs, **kwargs))
     elif obj_dims == axis_dims == 3:
-        return _plot_3d(ax, layout, cga_objs, **kwargs)
+        return list(_Plotter3d(ax, layout).plot(cga_objs, **kwargs))
     elif obj_dims == axis_dims:
         raise NotImplementedError("Cannot plot {}-D objects".format(obj_dims))
     elif obj_dims != axis_dims:
         raise TypeError("Objects live in {}-D space, but the axes are {}-D".format(obj_dims, axis_dims))
 
 
-def _as_point_tuple_2d(p):
-    return p[(1,)], p[(2,)]
+def _groupby(l, key=lambda x: x):
+    d = collections.defaultdict(list)
+    for item in l:
+        d[key(item)].append(item)
+    return d.items()
 
 
-def _plot_2d(ax, _layout, cga_objs, **kwargs):
-    os = [classify.classify(cga_obj) for cga_obj in cga_objs]
+class _Plotter:
+    def __init__(self, ax, layout):
+        self._layout = layout
+        self._ax = ax
 
-    if all(isinstance(o, classify.Point) for o in os):
-        x, y = zip(*[_as_point_tuple_2d(point.location) for point in os])
-        return ax.plot(x, y, **kwargs)
-    elif all(isinstance(o, classify.Tangent[2]) for o in os):
-        patches = []
+    def plot(self, cga_objs, **kwargs) -> Iterator[Artist]:
+        all_os = [classify.classify(cga_obj) for cga_obj in cga_objs]
+        for handler, os in _groupby(all_os, lambda o: functools._find_impl(type(o), self._handlers)):
+            if not handler:
+                continue  # skipped
+            yield from handler(self, os, **kwargs)
+
+
+class _Plotter2d(_Plotter):
+    _handlers = {}
+
+    def _handles(t, _handlers=_handlers):
+        def decorator(f):
+            _handlers[t] = f
+            return f
+        return decorator
+
+    @classmethod
+    def _as_point_tuple_2d(cls, p):
+        return p[(1,)], p[(2,)]
+
+    @_handles(classify.Point)
+    def _plot_Point(self, os, **kwargs) -> Iterator[Artist]:
+        x, y = zip(*[self._as_point_tuple_2d(point.location) for point in os])
+        yield from self._ax.plot(x, y, **kwargs)
+
+    @_handles(classify.Tangent[2])
+    def _plot_Tangent(self, os, **kwargs) -> Iterator[Artist]:
         for tangent in os:
             p = FancyArrowPatch(
-                _as_point_tuple_2d(tangent.location),
-                _as_point_tuple_2d(tangent.location + tangent.direction.normal()),
+                self._as_point_tuple_2d(tangent.location),
+                self._as_point_tuple_2d(tangent.location + tangent.direction.normal()),
                 arrowstyle="->", shrinkA=0, shrinkB=0, mutation_scale=10
             )
-            ax.add_patch(p)
-            patches.append(p)
-        return patches
-    elif all(isinstance(o, classify.PointPair) for o in os):
-        ret = []
+            self._ax.add_patch(p)
+            yield p
+
+    @_handles(classify.PointPair)
+    def _plot_PointPair(self, os, **kwargs) -> Iterator[Artist]:
         for o in os:
             d = o.direction.normal()
             a = o.location - abs(o.radius) * d
             b = o.location + abs(o.radius) * d
-            x, y = zip(*[_as_point_tuple_2d(end) for end in (a, b)])
-            ret.append(ax.plot(x, y, linestyle=_line_style_for_radius(o.radius), **kwargs))
-        return ret
-    elif all(isinstance(o, classify.Line) for o in os):
-        return [
-            ax.axline(
-                _as_point_tuple_2d(line.location - line.direction / 2),
-                _as_point_tuple_2d(line.location + line.direction / 2),
+            x, y = zip(*[self._as_point_tuple_2d(end) for end in (a, b)])
+            yield from self._ax.plot(x, y, linestyle=_line_style_for_radius(o.radius), **kwargs)
+
+    @_handles(classify.Line)
+    def _plot_Line(self, os, **kwargs) -> Iterator[Artist]:
+        for line in os:
+            yield self._ax.axline(
+                self._as_point_tuple_2d(line.location - line.direction / 2),
+                self._as_point_tuple_2d(line.location + line.direction / 2),
                 **kwargs
             )
-            for line in os
-        ]
-    elif all(isinstance(o, classify.Circle) for o in os):
+
+    @_handles(classify.Circle)
+    def _plot_Circle(self, os, **kwargs) -> Iterator[Artist]:
         # adjust the color arguments to make sense
         kwargs.setdefault('facecolors', 'none')
         try:
@@ -99,21 +136,15 @@ def _plot_2d(ax, _layout, cga_objs, **kwargs):
         else:
             kwargs['edgecolors'] = color
         c = [
-            Circle(_as_point_tuple_2d(circle.location), abs(circle.radius))
+            Circle(self._as_point_tuple_2d(circle.location), abs(circle.radius))
             for circle in os
         ]
         linestyles = [_line_style_for_radius(circle.radius) for circle in os]
         col = PatchCollection(c, linestyles=linestyles, **kwargs)
-        print(col.get_edgecolors())
-        ax.add_collection(col)
-        ax._request_autoscale_view()
-        return col
-    else:
-        raise NotImplementedError
+        self._ax.add_collection(col)
+        self._ax._request_autoscale_view()
+        yield col
 
-
-def _as_point_tuple_3d(p):
-    return p[(1,)], p[(2,)], p[(3,)]
 
 
 class Arrow3D(FancyArrowPatch):
@@ -135,7 +166,6 @@ class Circle3D(Patch):
         path = Path.unit_circle()
         self.radius = radius
         self._code3d = path.codes
-        print(matrix.shape)
         self._segment3d = center + radius * (path.vertices @ matrix.T)
         self._path2d = Path(np.zeros((0, 2)))
 
@@ -154,35 +184,50 @@ class Circle3D(Patch):
     #     super().draw(self, renderer)
 
 
-def _plot_3d(ax, _layout, cga_objs, **kwargs):
-    os = [classify.classify(cga_obj) for cga_obj in cga_objs]
+class _Plotter3d(_Plotter):
+    _handlers = {}
 
-    if all(isinstance(o, classify.Point) for o in os):
-        coords = zip(*[_as_point_tuple_3d(point.location) for point in os])
-        return ax.plot(*coords, **kwargs)
-    elif all(isinstance(o, classify.Tangent[2]) for o in os):
-        patches = []
+    def _handles(t, _handlers=_handlers):
+        def decorator(f):
+            _handlers[t] = f
+            return f
+        return decorator
+
+    @classmethod
+    def _as_point_tuple_3d(cls, p):
+        return p[(1,)], p[(2,)], p[(3,)]
+
+    @_handles(classify.Point)
+    def _plot_Point(self, os, **kwargs) -> Iterator[Artist]:
+        coords = zip(*[self._as_point_tuple_3d(point.location) for point in os])
+        return self._ax.plot(*coords, **kwargs)
+
+    @_handles(classify.Tangent[2])
+    def _plot_Tangent(self, os, **kwargs) -> Iterator[Artist]:
         for tangent in os:
             p = Arrow3D(
-                _as_point_tuple_3d(tangent.location),
-                _as_point_tuple_3d(tangent.location + tangent.direction.normal()),
+                self._as_point_tuple_3d(tangent.location),
+                self._as_point_tuple_3d(tangent.location + tangent.direction.normal()),
                 arrowstyle="->", shrinkA=0, shrinkB=0, mutation_scale=10
             )
-            ax.add_patch(p)
-            patches.append(p)
-        return patches
-    elif all(isinstance(o, classify.PointPair) for o in os):
-        ret = []
+            self._ax.add_patch(p)
+            yield p
+
+    @_handles(classify.PointPair)
+    def _plot_PointPair(self, os, **kwargs) -> Iterator[Artist]:
         for o in os:
             d = o.direction.normal()
             a = o.location - abs(o.radius) * d
             b = o.location + abs(o.radius) * d
-            coords = zip(*[_as_point_tuple_3d(end) for end in (a, b)])
-            ret.append(ax.plot(*coords, linestyle=_line_style_for_radius(o.radius), **kwargs))
-        return ret
-    elif all(isinstance(o, classify.Line) for o in os):
+            coords = zip(*[self._as_point_tuple_3d(end) for end in (a, b)])
+            yield from self._ax.plot(*coords, linestyle=_line_style_for_radius(o.radius), **kwargs)
+
+    @_handles(classify.Line)
+    def _plot_Line(self, os, **kwargs) -> Iterator[Artist]:
         raise NotImplementedError
-    elif all(isinstance(o, classify.Circle) for o in os):
+
+    @_handles(classify.Circle)
+    def _plot_Circle(self, os, **kwargs) -> Iterator[Artist]:
         # adjust the color arguments to make sense
         kwargs.setdefault('facecolor', 'none')
         try:
@@ -192,27 +237,26 @@ def _plot_3d(ax, _layout, cga_objs, **kwargs):
         else:
             kwargs['edgecolor'] = color
 
-        e1, e2, e3 = _layout.blades['e1'], _layout.blades['e2'], _layout.blades['e3']
+        e1, e2, e3 = self._layout.blades['e1'], self._layout.blades['e2'], self._layout.blades['e3']
         ret = []
         for circle in os:
             rotor = (circle.direction.normal() * (e1 ^ e2) + 1).normal()
             mat = clifford.linear_operator_as_matrix(lambda x: rotor * x * ~rotor, [e1, e2], [e1, e2, e3])
             p = Circle3D(
-                _as_point_tuple_3d(circle.location),
+                self._as_point_tuple_3d(circle.location),
                 abs(circle.radius), mat,
                 linestyle=_line_style_for_radius(circle.radius),
                 **kwargs
             )
-            ax.add_patch(p)
-            ret.append(p)
-        return ret
-    elif all(isinstance(o, classify.Sphere) for o in os):
+            self._ax.add_patch(p)
+            yield p
+
+    @_handles(classify.Sphere)
+    def _plot_Sphere(self, os, **kwargs) -> Iterator[Artist]:
+        from trimesh.creation import icosphere
         kwargs.setdefault('alpha', 0.5)
         ret = []
         for o in os:
             tmesh = icosphere(radius=abs(o.radius))
             t = Triangulation(tmesh.vertices[:, 0], tmesh.vertices[:, 1], triangles=tmesh.faces)
-            ret.append(ax.plot_trisurf(t, tmesh.vertices[:, 2], **kwargs))
-        return ret
-    else:
-        raise NotImplementedError
+            yield self._ax.plot_trisurf(t, tmesh.vertices[:, 2], **kwargs)
