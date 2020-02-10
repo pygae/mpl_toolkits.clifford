5c25ddc60389c3251a23e6e0cc195649638eaac1
Eric Wieser <wieser.eric@gmail.com>
2020-02-10 12:26:41 +0000

    Convert a ganja.js example by @hugohadfield to use this package

---
+++ examples/serial_manipulator.py
@@ -0,0 +1,63 @@
+# from https://enkimute.github.io/ganja.js/examples/coffeeshop.html#AFVs70eea
+
+import math
+
+from matplotlib import pyplot as plt
+from matplotlib import animation
+import numpy as np
+import mpl_toolkits.clifford
+
+from clifford.g3c import *
+
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1, projection="3d")
+ax.set(xlim=[-1, 1], ylim=[0, 2], zlim=[-1, 1], autoscale_on=False)
+
+def basic_animation(fig, *, interval, **kwargs):
+    def decorator(f):
+        last_artists = []
+        def animate(i):
+            for a in last_artists:
+                a.remove()
+            last_artists[:] = list(f(i*interval))
+            return last_artists
+
+        return animation.FuncAnimation(
+            fig, animate, init_func=lambda: animate(0), **kwargs, interval=interval*1000)
+
+    return decorator
+
+
+# Draw a robot base
+C = up(0.2*e1)^up(0.2*e3)^up(-0.2*e1);
+
+@basic_animation(fig, interval=0.005, save_count=100)
+def animation(t):
+    # Set up time varying parameters for the the rotors
+    speed = 4
+    theta0 = math.cos(speed*t/3)*math.pi/2
+    theta1 = math.cos(speed*t/1)*math.pi/4
+    theta2 = math.cos(speed*t/0.5)*math.pi/2
+
+    # Create a time varying rotor to mimic the behaviour of the base
+    R0 = (-theta0*e13/2).exp()
+    R1 = (-theta1*e12/2).exp()
+    Rbase = (R0*R1).normal()
+
+    #  Define the points of the robot
+    elb0 = Rbase*e2*(~Rbase)
+
+    # This is the motor in the elbow
+    R2 = Rbase*(-theta2*e12/2).exp()
+
+    # The end point
+    endpoint = elb0 + R2*0.5*e2*~R2
+
+    yield from mpl_toolkits.clifford.plot(ax, [up(0), R0*up(0.2*e1)*~R0], color='tab:blue')
+    yield from mpl_toolkits.clifford.plot(ax, [C], color='tab:blue')
+    yield from mpl_toolkits.clifford.plot(ax, [up(0), up(elb0), up(endpoint)], marker='x', color='tab:orange')
+
+# animation.save('test2.gif', writer='imagemagick')
+
+plt.show()
