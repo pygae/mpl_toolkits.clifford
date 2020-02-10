104717fabc21d11392742b684784f0810ea2a6cc
Eric Wieser <wieser.eric@gmail.com>
2020-02-10 13:30:35 +0000

    Use readme on PyPI

--- setup.py
+++ setup.py
@@ -6,18 +6,17 @@ import os
 version_path = os.path.join('mpl_toolkits', 'clifford', '_version.py')
 exec(open(version_path).read())
 
-LONG_DESCRIPTION = """
-A package to provide tools for plotting 2D and 3D GA in ``matplotlib``.
-
-Often a
-"""
+this_directory = os.path.dirname(__file__)
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='mpl_toolkits.clifford',
     version=__version__,
     license='MIT',
     description='Matplotlib tools for clifford',
-    long_description=LONG_DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Eric Wieser',
     py_modules=['mpl_toolkits.clifford'],
     namespace_packages=['mpl_toolkits'],
